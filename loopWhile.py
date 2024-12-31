2cfaa57348ab1c56c85f1739ae390acb433b4152
Priscilaz <priscila.zuniga@udla.edu.ec>
2024-12-30 21:37:29 -0500

    Rangos

--- loopWhile.py
+++ loopWhile.py
@@ -53,3 +53,4 @@ while numero>=0:
 
 
 
+
---
+++ rango.py
@@ -0,0 +1,21 @@
+for numero in range(5):
+    print(numero)
+print("*************")
+for numero in range(1,5):
+    print(numero)
+print("*************")
+for numero in range(1,15,5):
+    print(numero)
+
+
+##-- Hcaer una lista que vaya del 1 al 100
+lista = list(range(1,101))
+print(lista)
+
+
+"""Utilizando la función range(), 
+crea en una única linea de código una 
+lista formada por todos los números múltiplos 
+de 3 desde el 3 al 300 (inclusive). Almacena 
+dicha lista en la variable mi_lista."""
+mi_lista = list(range(3,301,3))
