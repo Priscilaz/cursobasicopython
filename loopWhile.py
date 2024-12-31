197b313361d21ec30bb5b0fec4795ff90ba6537f
Priscilaz <priscila.zuniga@udla.edu.ec>
2024-12-30 21:20:43 -0500

    Loop WHILE

---
+++ loopWhile.py
@@ -0,0 +1,55 @@
+"""monedas = 5
+
+while monedas>0:
+    print(f"Tengo {monedas} monedas")
+    monedas -= 1
+
+else:
+    print("No tengo mas dinero")"""
+
+'''respuesta = 's'
+
+while respuesta == 's':
+    respuesta = input("quieres seguir? s/n: ")
+
+else:
+    print("Graciaaaaaassss!!!")'''
+
+"""respuesta = 's'
+
+while respuesta == 's':
+    pass
+print("hola")"""
+
+"""nombre = input("Tu nombre es: ")
+for letra in nombre:
+    if letra == 'r':
+        continue #break
+    print(letra)"""
+
+numero = 50
+
+while numero>=0:
+    if numero % 5 == 0:
+        print(numero)
+
+    numero -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
