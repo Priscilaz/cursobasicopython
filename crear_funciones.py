dd07e9e01b1399f3ee0d1235922cd13438448198
Priscilaz <priscila.zuniga@udla.edu.ec>
2025-01-02 11:52:50 -0500

    Creacion de Funciones

---
+++ crear_funciones.py
@@ -0,0 +1,28 @@
+"""def saludar_persona (nombre):
+
+    Esta funcion permite saludar
+    al usuario
+
+    print("Hola " + nombre)
+
+saludar_persona('Priscila') """
+
+"""def multiplicar(n1, n2):
+    return n1*n2
+
+resultado = multiplicar(2,4)
+print(resultado)"""
+
+
+"""def potencia(n1, n2):
+
+
+    return n1 ** n2
+potencia(2,3)"""
+
+palabra = 'Priscila'
+def invertir_palabra(palabra):
+    p = palabra[::-1].upper()
+    print(p)
+    return p
+invertir_palabra(palabra)
